d23fddb0473d2e2f5c1c70b9f414d314dd414442
Muhammad ElNa7tta <Mohamed.abdulla.alnahtta.1@gmail.com>
2021-08-28 14:47:43 +0200

    update

--- database/db.sql
+++ database/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2021 at 09:12 PM
+-- Generation Time: Aug 28, 2021 at 02:45 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.4
 
@@ -35,20 +35,6 @@ CREATE TABLE `login-sessions` (
   `loginTme` datetime DEFAULT NULL,
   `loginOut` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `login-sessions`
---
-
-INSERT INTO `login-sessions` (`id`, `username`, `token`, `loginTme`, `loginOut`) VALUES
-(1, 'ahmed', 'sada', '2021-08-27 00:00:00', '2021-08-27 21:11:47'),
-(2, 'ahmed', 'token', '2021-08-27 20:55:49', '2021-08-27 21:11:47'),
-(3, 'ahmed', 'token', '2021-08-27 21:00:10', '2021-08-27 21:11:47'),
-(4, 'ahmed', 'token', '2021-08-27 21:00:13', '2021-08-27 21:11:47'),
-(5, 'ahmed', '09d6da294fb1efd1d088f0d93e7720d6kbcVVILMRIiRfRvg2lg4wWVVQibQomntwVRkc5vXCGwaQ8GbeW', '2021-08-27 21:00:29', '2021-08-27 21:11:47'),
-(6, 'mohamed', 'a337b70f17380fd2b092f349140ca2e8iwiivsqXv3PnjWJ7raqLN2Nslxy44UlaefCFkiArx7qA6tzzaC', '2021-08-27 21:01:36', '2021-08-27 00:00:00'),
-(7, 'ahmed', '58ef5626308f4d26b86a25998dae5384QH38NNfLb8Zi4mR7c6mfzGYGUR8hUsUPcQSBHLpjBzYsYPKMQo', '2021-08-27 21:11:15', '2021-08-27 21:11:47'),
-(8, 'ahmed', '436a102527dbf8c8c66b041ac7e1f04d4M0LTxqBz2Rw6DiMe25CX50OADbA092mWQNQxLZkPoC2JZcr5D', '2021-08-27 21:11:50', NULL);
 
 -- --------------------------------------------------------
 
@@ -98,7 +84,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `login-sessions`
 --
 ALTER TABLE `login-sessions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
--- include/config.php
+++ include/config.php
@@ -1,7 +1,14 @@
 <?php
 
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "prvent-multiple-sessions-login";
 
+// Create connection
+$connection = mysqli_connect($servername, $username, $password, $dbname);
 
-
-
-
+// Check connection
+if (!$connection) {
+    die("Connection failed: Please Contact Your Administrator ");
+}
