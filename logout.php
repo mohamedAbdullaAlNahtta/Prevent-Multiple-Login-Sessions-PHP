f46e2b7fc5582d06fbfeb38979a8d1bf159a2e1a
Muhammad ElNa7tta <Mohamed.abdulla.alnahtta.1@gmail.com>
2021-08-28 14:20:51 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,39 +2,13 @@
 session_start();
 error_reporting(0);
 
-/* arabicss database connection */
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "prvent-multiple-sessions-login";
-
-// Create connection
-$connection = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$connection) {
-    die("Connection failed: Please Contact Your Administrator ");
-}
-
-
-$logoutdate=date(' Y-m-d H:i:s ',strtotime('+1 hour ',strtotime(date('Y-m-d H:i:s '))));
-
-$username=$_SESSION['username'];
-
-
-mysqli_query($connection,"UPDATE `user_log`  SET `logoutTime` = NOW() WHERE `username` = '$username' ORDER BY id DESC LIMIT 1");
-
 $_SESSION['username']=="";
+$_SESSION['password']=="";
+$_SESSION['token']=="";
+
 session_destroy();
 session_unset();
 
-
-/* database destroy connection */
-
-mysqli_close($connection);
+header("location: login.php");
 
 ?>
-<!-- <script language="javascript">
-document.location="login.php";
-</script>
- -->
