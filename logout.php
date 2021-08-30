3a2d07aef522c4debcb700dd92ccf45888d1a987
Muhammad ElNa7tta <Mohamed.abdulla.alnahtta.1@gmail.com>
2021-08-30 22:27:58 +0200

    update

--- logout.php
+++ logout.php
@@ -6,8 +6,13 @@ $_SESSION['username']=="";
 $_SESSION['password']=="";
 $_SESSION['token']=="";
 
+$errormsg=$_SESSION['errormsg'];
 session_destroy();
 session_unset();
+
+session_start();
+error_reporting(0);
+$_SESSION['errormsg']=;
 
 header("location:login.php");
 
